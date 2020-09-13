9b38f31649ff42c7730706e798d9713e8e929d14
piljupark <62000236+piljupark@users.noreply.github.com>
2020-09-13 18:37:31 +0900

    200913-5

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var scroll = $(window).scrollTop(); 
     if(scroll>1){ 
       $("header").css("background","rgba(0, 0, 0, .8)"); 
+ 
     } 
     else{ 
       $("header").css("background","rgba(0, 0, 0, .1)"); 

aef17d64fa9ea719009827ee2a83cf14c7eb8201
piljupark <62000236+piljupark@users.noreply.github.com>
2020-10-06 00:32:37 +0900

    201006-1

--- script/script.js
+++ script/script.js
@@ -9,4 +9,11 @@ $(document).ready(function(){
       $("header").css("background","rgba(0, 0, 0, .1)"); 
     }
   });
+    $("#wrap > header > .model-logo").mouseenter(function(){
+        $("#model-form").show(300);
+    });
+     $("#model-form > .model-list > a > .car").mouseenter(function(){
+        $("#int-car").show(300);
+        $("#model-form").hide();
+    });
 });

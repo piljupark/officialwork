e63a471c7042d777cc0369422f37ba42f373a042
piljupark <62000236+piljupark@users.noreply.github.com>
2021-05-20 16:53:52 +0900

    210520-3
    support 작성 중

--- script/script.js
+++ script/script.js
@@ -835,38 +835,6 @@ $(document).ready(function () {
     /*     //maintain     */
 
 
-
-
-
-
-    $("#support > .support-main-img-wrap > .support-img2").click(function () {
-        $("#support > .support-main-img-wrap > .support-img1").css("width", "25%");
-        $("#support > .support-main-img-wrap > .support-img1").css("opacity", "0.3");
-        $("#support > .support-main-img-wrap > .support-img1 > p").css("font-size", "20px");
-        $("#support > .support-main-img-wrap > .support-img1 > p").css("margin-top", "125px");
-        $("#support > .support-main-img-wrap > .support-img2").css("opacity", "1");
-        $("#support > .support-main-img-wrap > .support-img2 > p").css("font-size", "40px");
-        $("#support > .support-main-img-wrap > .support-img2").css("width", "70%");
-        $("#support > .support-main-img-wrap > .support-img2 > p").css("margin-top", "100px");
-        $("#support > .support-ex-wrapper2").css("display", "block");
-        $("#support > .support-ex-wrapper1").css("display", "none");
-        $("#support > .support-caution-wrap").css("display", "none");
-    });
-    $("#support > .support-main-img-wrap > .support-img1").click(function () {
-        $("#support > .support-main-img-wrap > .support-img2").css("width", "25%");
-        $("#support > .support-main-img-wrap > .support-img2").css("opacity", "0.3");
-        $("#support > .support-main-img-wrap > .support-img2 > p").css("font-size", "20px");
-        $("#support > .support-main-img-wrap > .support-img2 > p").css("margin-top", "115px");
-        $("#support > .support-main-img-wrap > .support-img1").css("opacity", "1");
-        $("#support > .support-main-img-wrap > .support-img1 > p").css("font-size", "40px");
-        $("#support > .support-main-img-wrap > .support-img1").css("width", "70%");
-        $("#support > .support-main-img-wrap > .support-img1 > p").css("margin-top", "115px");
-        $("#support > .support-ex-wrapper1").css("display", "block");
-        $("#support > .support-ex-wrapper2").css("display", "none");
-        $("#support > .support-caution-wrap").css("display", "block");
-    });
-
-
     $("#promotion > .promotion-wrap > .promotion-img-wrap1").hover(function () {
 
         $("#promotion > .promotion-wrap > .promotion-img-wrap1 > .promotion-txt").css("display", "none");

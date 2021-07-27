19e9896e551ec15cbf24c3cda81647c4d68acbc2
piljupark <62000236+piljupark@users.noreply.github.com>
2021-07-27 23:31:46 +0900

    210727-6

--- aesop-script/aesop.js
+++ aesop-script/aesop.js
@@ -16,10 +16,31 @@ $(document).ready(function () {
        $(".mouse").css("animation-name","fadeOut"); 
         
         
+        $(".top-menu").css("opacity","0"); 
+       $(".top-menu").css("animation-name","otherfadeOut"); 
+       $(".top-menu").css("animation-delay",".5s"); 
+       $(".top-menu").css("animation-fill-mode","forwards");
+        
+        
+         $(".product-place").css("opacity","0"); 
+       $(".product-place").css("animation-name","otherfadeOut"); 
+       $(".product-place").css("animation-delay",".5s"); 
+       $(".product-place").css("animation-fill-mode","forwards");
+        
+        
+        $(".athenaeum").css("opacity","0"); 
+       $(".athenaeum").css("animation-name","otherfadeOut"); 
+       $(".athenaeum").css("animation-delay",".5s"); 
+       $(".athenaeum").css("animation-fill-mode","forwards");
+        
+        
         $(".store-place").css("animation-name","storefadeIn");
         $(".store-place").css("animation-duration",".8s");
         $(".store-place").css("animation-delay",".4s");
         $(".store-place").css("animation-fill-mode","forwards");
+        
+        
+        $("body").css("overflow-y","hidden");
     });
     
 });

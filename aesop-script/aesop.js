0d02e2588ef8eadb7a0a50f6357a2e1c94e23c38
piljupark <62000236+piljupark@users.noreply.github.com>
2021-07-27 15:55:53 +0900

    210727-3

--- aesop-script/aesop.js
+++ aesop-script/aesop.js
@@ -8,4 +8,12 @@ $(document).ready(function () {
        $("#search-place").css("height","0px"); 
     });
     
+    $("#store").click(function(){
+       $(".swiper-container").css("opacity","0"); 
+       $(".swiper-container").css("animation-name","otherfadeOut"); 
+       $(".swiper-container").css("animation-delay",".5s"); 
+       $(".swiper-container").css("animation-fill-mode","forwards"); 
+       $(".mouse").css("animation-name","fadeOut"); 
+    });
+    
 });

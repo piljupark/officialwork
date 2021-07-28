bfb0fe599d8dfca1c4aa9fbab57205fc8a6d694d
piljupark <62000236+piljupark@users.noreply.github.com>
2021-07-28 15:29:56 +0900

    210728-4

--- aesop-script/aesop.js
+++ aesop-script/aesop.js
@@ -3,53 +3,73 @@ $(document).ready(function () {
     $("#search").click(function () {
         $(".search-place").css("left", "240px");
         $(".bg-black").css("display", "block");
+        
+        $(".store-place").css("left", "-240px");
+        
+        $("body").css("overflow-y","hidden");
     });
 
     $(".prev").click(function () {
         $(".search-place").css("left", "-240px");
         $(".bg-black").css("display", "none");
+        
+        $("body").css("overflow-y","auto");
     });
-
+    
+    
+    
     $("#store").click(function () {
-        $(".swiper-container").css("opacity", "0");
-        $(".swiper-container").css("animation-name", "otherfadeOut");
-        $(".swiper-container").css("animation-delay", ".5s");
-        $(".swiper-container").css("animation-fill-mode", "forwards");
-        $(".mouse").css("animation-name", "fadeOut");
-
-
-        $(".top-menu").css("opacity", "0");
-        $(".top-menu").css("animation-name", "otherfadeOut");
-        $(".top-menu").css("animation-delay", ".5s");
-        $(".top-menu").css("animation-fill-mode", "forwards");
-
-
-        $(".product-place").css("opacity", "0");
-        $(".product-place").css("animation-name", "otherfadeOut");
-        $(".product-place").css("animation-delay", ".5s");
-        $(".product-place").css("animation-fill-mode", "forwards");
-
-
-        $(".athenaeum").css("opacity", "0");
-        $(".athenaeum").css("animation-name", "otherfadeOut");
-        $(".athenaeum").css("animation-delay", ".5s");
-        $(".athenaeum").css("animation-fill-mode", "forwards");
-
-
-        $(".store-place").css("animation-name", "storefadeIn");
-        $(".store-place").css("animation-duration", ".8s");
-        $(".store-place").css("animation-delay", ".4s");
-        $(".store-place").css("animation-fill-mode", "forwards");
-
-
-        $("body").css("overflow-y", "hidden");
+        $(".store-place").css("left", "240px");
+        $(".bg-black").css("display", "block");
+        
+        $(".search-place").css("left", "-240px");
+        
+        $("body").css("overflow-y","hidden");
     });
 
+    $(".prev").click(function () {
+        $(".store-place").css("left", "-240px");
+        $(".bg-black").css("display", "none");
+        
+        
+        $("body").css("overflow-y","auto");
+    });
+
+    
+
+    
+    
+    
+    /* 태블릿 스크립트 */
     if (document.body.clientWidth < 1021) {
+        
+        
+        $("#store").click(function () {
+            $(".store-place-mob").css("top", "90px");
+            $(".store-place-mob").css("left", "0px");
+            
+            $(".search-place-mob").css("top", "-460px");
+        
+            $(".bg-black").css("display", "block");
+            $("body").css("overflow","hidden");
+        });
+
+        $(".prev").click(function () {
+            $(".store-place-mob").css("top", "-460px");
+            $(".store-place-mob").css("left", "0px");
+            $(".bg-black").css("display", "none");
+            $("body").css("overflow-y","auto");
+        });
+        
+        
+        
 
         $("#search").click(function () {
             $(".search-place-mob").css("top", "90px");
             $(".search-place-mob").css("left", "0px");
+            
+            $(".store-place-mob").css("top", "-460px");
+            
             $(".bg-black").css("display", "block");
             $("body").css("overflow","hidden");
         });
@@ -60,8 +80,10 @@ $(document).ready(function () {
             $(".bg-black").css("display", "none");
             $("body").css("overflow-y","scroll");
         });
+        
+  
     }
-
+    /* //태블릿 스크립트 */
 
 
 

020f28566fde012bf5efa23962268c3546fadfb4
piljupark <62000236+piljupark@users.noreply.github.com>
2021-08-14 22:33:16 +0900

    210814-14

--- plantlance1-script/plantlance1.js
+++ plantlance1-script/plantlance1.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     
     if (document.body.clientWidth < 701) {
     $("nav > .nav-wrap > .mobile-menu > a > .mobile-spread").click(function(){
-        $("body").css("overflow","hidden");
         
         $(".spread-mobile-menu").css("height","100vh");
         
@@ -14,7 +13,6 @@ $(document).ready(function () {
     
     
     $("nav > .nav-wrap > .mobile-menu > a > .mobile-close").click(function(){
-        $("body").css("overflow-y","auto");
         
         $(".spread-mobile-menu").css("height","0vh");
         

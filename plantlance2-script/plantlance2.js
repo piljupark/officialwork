4424bf4d9cd4f49ef35b43d6d5f17e18bcc28c62
piljupark <62000236+piljupark@users.noreply.github.com>
2021-08-21 18:01:27 +0900

    210821-1

--- plantlance2-script/plantlance2.js
+++ plantlance2-script/plantlance2.js
@@ -1,12 +1,30 @@
 $(document).ready(function () {
-    
-   
-    $("nav > .nav-wrap > ul > a > li").mouseenter(function(){
-        $("nav > .nav-wrap").css("height","300px");
+    $(".global-menu-button").click(function(){
+        $("body").css("overflow-y","hidden");
+        
+        $("nav").css("z-index","8");
+        
+        $("#spread-menu").css("right","0");
+        $("#click-to-black").css("opacity","1");
+        
+      
+        
     });
-    $("nav > .nav-wrap > ul > a > li").mouseleave(function(){
-        $("nav > .nav-wrap").css("height","50px");
-    });
    
+    $(".close-button").click(function(){
+        $("body").css("overflow-y","auto");
+        
+        $("nav").css("z-index","10");
+        
+        $("#spread-menu").css("right","-300px");
+        $("#click-to-black").css("opacity","0");
+        
+        
+        $("#click-to-black").css("animation-name","blackToback");        
+        $("#click-to-black").css("animation-delay",".6s");        
+        $("#click-to-black").css("animation-fill-mode","forwards"); 
+        
+        
+    });
 
 })

9b23402eb4bcfaa5b06e85c4b03a18b1e3e6cc1f
piljupark <62000236+piljupark@users.noreply.github.com>
2021-08-27 19:02:23 +0900

    210827-4

--- plantlance3-script/plantlance3.js
+++ plantlance3-script/plantlance3.js
@@ -13,10 +13,10 @@ $(function () {
 
     
     $("nav > .nav-wrap > .menu-circle > a > .menu-btn").click(function(){
-       $("nav > .spread-menu").css("top","0"); 
+       $("nav > .spread-menu").css("width","500px"); 
     });
     
     $("nav > .spread-menu > .spread-menu-wrap > a > .menu-close-btn").click(function(){
-       $("nav > .spread-menu").css("top","-500px"); 
+       $("nav > .spread-menu").css("width","0px"); 
     });
 });

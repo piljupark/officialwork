173633ca21f0de8c4bc9a82d25436ee27049a710
piljupark <62000236+piljupark@users.noreply.github.com>
2021-08-31 18:43:39 +0900

    210831-2

--- plantlance2-script/plantlance2.js
+++ plantlance2-script/plantlance2.js
@@ -1,64 +1,132 @@
 $(document).ready(function () {
-   $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").click(function(){
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity","1");
-       
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity",".6");
-   });
+    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").click(function () {
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity", "1");
+
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity", ".6");
+
+
+        $(".showroom-gimpo-wrap").css("display", "block");
+        $(".showroom-daegu-wrap").css("display", "none");
+        $(".showroom-seongsu-wrap").css("display", "none");
+        $(".showroom-sejong-wrap").css("display", "none");
+        $(".showroom-pangyo-wrap").css("display", "none");
+        $(".showroom-busan-wrap").css("display", "none");
+        
+        
+    });
+
+
+    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").click(function () {
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity", "1");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity", ".6");
+
+        $(".showroom-gimpo-wrap").css("display", "none");
+        $(".showroom-daegu-wrap").css("display", "block");
+        $(".showroom-seongsu-wrap").css("display", "none");
+        $(".showroom-sejong-wrap").css("display", "none");
+        $(".showroom-pangyo-wrap").css("display", "none");
+        $(".showroom-busan-wrap").css("display", "none");
+        
+        
+    });
+
+
+    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").click(function () {
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity", ".6");
+
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity", "1");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity", ".6");
+
+        
+        
+        $(".showroom-gimpo-wrap").css("display", "none");
+        $(".showroom-daegu-wrap").css("display", "none");
+        $(".showroom-seongsu-wrap").css("display", "block");
+        $(".showroom-sejong-wrap").css("display", "none");
+        $(".showroom-pangyo-wrap").css("display", "none");
+        $(".showroom-busan-wrap").css("display", "none");
+        
+        
+    });
+
+    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").click(function () {
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity", ".6");
+
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity", "1");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity", ".6");
+        
+        
+        
+
+
+
+        $(".showroom-gimpo-wrap").css("display", "none");
+        $(".showroom-daegu-wrap").css("display", "none");
+        $(".showroom-seongsu-wrap").css("display", "none");
+        $(".showroom-sejong-wrap").css("display", "block");
+        $(".showroom-pangyo-wrap").css("display", "none");
+        $(".showroom-busan-wrap").css("display", "none");
+        
+        
+        
+    });
+
+    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").click(function () {
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity", ".6");
+
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity", "1");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity", ".6");
+
+
+        $(".showroom-gimpo-wrap").css("display", "none");
+        $(".showroom-daegu-wrap").css("display", "none");
+        $(".showroom-seongsu-wrap").css("display", "none");
+        $(".showroom-sejong-wrap").css("display", "none");
+        $(".showroom-pangyo-wrap").css("display", "block");
+        $(".showroom-busan-wrap").css("display", "none");
+        
+        
     
-    
-    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").click(function(){
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity","1");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity",".6");
-   });
-    
-    
-    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").click(function(){
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity",".6");
-       
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity","1");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity",".6");
-   });
-    
-    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").click(function(){
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity",".6");
-       
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity","1");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity",".6");
-   });
-    
-    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").click(function(){
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity",".6");
-       
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity","1");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity",".6");
-   });
-    
-    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").click(function(){
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity",".6");
-       
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity",".6");
-       $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity","1");
-   });
-    
-    
+    });
+
+    $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").click(function () {
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(1) > p").css("opacity", ".6");
+
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(2) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(3) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(4) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(5) > p").css("opacity", ".6");
+        $("#showroom-section > .showroom-section-wrap > .showroom-txt-box > .showroom-place-txt-wrap > a:nth-child(6) > p").css("opacity", "1");
+        
+        
+        $(".showroom-gimpo-wrap").css("display", "none");
+        $(".showroom-daegu-wrap").css("display", "none");
+        $(".showroom-seongsu-wrap").css("display", "none");
+        $(".showroom-sejong-wrap").css("display", "none");
+        $(".showroom-pangyo-wrap").css("display", "none");
+        $(".showroom-busan-wrap").css("display", "block");
+        
+        
+        
+     
+    });
+
+
 })

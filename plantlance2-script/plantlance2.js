c043accb14e788b75238e332d3fe31f1ce5b09fd
piljupark <62000236+piljupark@users.noreply.github.com>
2021-09-02 16:09:07 +0900

    210902-5

--- plantlance2-script/plantlance2.js
+++ plantlance2-script/plantlance2.js
@@ -131,22 +131,35 @@ $(document).ready(function () {
     
     $("nav > .nav-wrap > .search-bg > .whole-search").click(function(){
         $("nav > .nav-wrap > .search-bg").css("height","400px");
-        $("nav > .nav-wrap > .search-bg > .whole-search").css("border","2px solid #fad02c");
+        $("nav > .nav-wrap > .search-bg > .whole-search").css("border","2px solid #e6c191");
         $("nav > .nav-wrap > .search-bg > .whole-search").css("width","85%");
-        $("nav > .nav-wrap > .search-bg > .close-btn").css("display","block");
+        
+        
+        $("nav > .nav-wrap > .search-bg > .close-wrap > a > .close-btn").css("display","block");
+        $("nav > .nav-wrap > .search-bg > .search-btn-wrap > a > .search-btn").css("display","block");
+        
         
         $(".bg-black").css("display","block");
+        
+        $("body").css("overflow","hidden");
     });
     
     
-    $("nav > .nav-wrap > .search-bg > a > .close-btn").click(function(){
+    $("nav > .nav-wrap > .search-bg > .close-wrap > a > .close-btn").click(function(){
         
-        $("nav > .nav-wrap > .search-bg").css("height","100px");
+        $("nav > .nav-wrap > .search-bg").css("height","90px");
         $("nav > .nav-wrap > .search-bg > .whole-search").css("border","none");
         $("nav > .nav-wrap > .search-bg > .whole-search").css("width","95%");
-        $("nav > .nav-wrap > .search-bg > .close-btn").css("display","none");
+        
+        
+        $("nav > .nav-wrap > .search-bg > .close-wrap > a > .close-btn").css("display","none");
+        $("nav > .nav-wrap > .search-bg > .search-btn-wrap > a > .search-btn").css("display","none");
+        
         
         $(".bg-black").css("display","none");
+        
+        
+        $("body").css("overflow-y","auto");
     });
 
 

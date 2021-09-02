0a35aa9392c44dd61f149fb9602bb90845d0cf02
piljupark <62000236+piljupark@users.noreply.github.com>
2021-09-02 18:03:24 +0900

    210902-6

--- plantlance2-script/plantlance2.js
+++ plantlance2-script/plantlance2.js
@@ -122,17 +122,22 @@ $(document).ready(function () {
         $(".showroom-sejong-wrap").css("display", "none");
         $(".showroom-pangyo-wrap").css("display", "none");
         $(".showroom-busan-wrap").css("display", "block");
-        
-        
-        
-     
     });
 
     
+    
+    /* 네비게이션 - 검색 버튼 활성화시 */
     $("nav > .nav-wrap > .search-bg > .whole-search").click(function(){
+        $("nav > .nav-wrap > .search-bg").css("background","#fff");
         $("nav > .nav-wrap > .search-bg").css("height","400px");
+        $("nav > .nav-wrap > .search-bg").css("transition-duration",".5s");
+        $("nav > .nav-wrap > .search-bg").css("position","absolute");
+        $("nav > .nav-wrap > .search-bg").css("margin-top","0px");
+        
+        
         $("nav > .nav-wrap > .search-bg > .whole-search").css("border","2px solid #e6c191");
         $("nav > .nav-wrap > .search-bg > .whole-search").css("width","85%");
+        
         
         
         $("nav > .nav-wrap > .search-bg > .close-wrap > a > .close-btn").css("display","block");
@@ -140,16 +145,26 @@ $(document).ready(function () {
         
         
         $(".bg-black").css("display","block");
+        $(".bg-black").css("z-index","9");
         
         $("body").css("overflow","hidden");
     });
     
     
+    
+    /* 네비게이션 - 검색 - 닫기 버튼 활성화시 */
     $("nav > .nav-wrap > .search-bg > .close-wrap > a > .close-btn").click(function(){
         
         $("nav > .nav-wrap > .search-bg").css("height","90px");
+        $("nav > .nav-wrap > .search-bg").css("background","none");
+        $("nav > .nav-wrap > .search-bg").css("transition-duration",".0s");
+        $("nav > .nav-wrap > .search-bg").css("position","relative");
+        $("nav > .nav-wrap > .search-bg").css("margin-top","5px");
+        
+        
         $("nav > .nav-wrap > .search-bg > .whole-search").css("border","none");
         $("nav > .nav-wrap > .search-bg > .whole-search").css("width","95%");
+         
         
         
         $("nav > .nav-wrap > .search-bg > .close-wrap > a > .close-btn").css("display","none");
@@ -157,9 +172,46 @@ $(document).ready(function () {
         
         
         $(".bg-black").css("display","none");
+        $(".bg-black").css("z-index","10");
         
         
         $("body").css("overflow-y","auto");
+    });
+    
+    
+    
+    
+    /* 네비게이션 - 메뉴 버튼 활성화시 */
+    $("nav > .nav-wrap > .nav-button-wrap > a > .menu-btn").click(function(){
+        $("body").css("overflow","hidden");
+        
+        $(".bg-black").css("display","block");
+        
+        $("#main-menu-slide").css("width","350px");
+        $("#main-menu-slide").css("overflow-y","scroll");
+        
+        $("#main-menu-slide > .main-menu-slide-wrap").css("opacity","1");
+        $("#main-menu-slide > .main-menu-slide-wrap").css("transition-delay",".26s");
+    });
+    
+    
+    
+    /* 네비게이션 - 메뉴 - 닫기 버튼 활성화시 */
+    $("#main-menu-slide > .main-menu-slide-wrap > a > .main-menu-close-btn").click(function(){
+        $("body").css("overflow-y","auto");
+        
+        $(".bg-black").css("display","none");
+        
+        $("#main-menu-slide").css("width","0px");
+        $("#main-menu-slide").css("overflow-y","hidden");
+        
+        $("#main-menu-slide > .main-menu-slide-wrap").css("opacity","0");
+        $("#main-menu-slide > .main-menu-slide-wrap").css("transition-delay","0s");
+        $("#main-menu-slide > .main-menu-slide-wrap").css("transition-duration",".1s");
+        
+        
+        
+        
     });
 
 

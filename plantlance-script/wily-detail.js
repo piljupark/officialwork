ec76f4b9c0c7e451962e6340e473f797978cde25
piljupark <62000236+piljupark@users.noreply.github.com>
2021-09-09 14:37:32 +0900

    210909-1

--- plantlance-script/wily-detail.js
+++ plantlance-script/wily-detail.js
@@ -91,4 +91,38 @@ $(document).ready(function () {
         $("#product-box > .product-box-wrap > .product-info-place > .choice-shape > .shape-kind > .shape-kind-box2 > a > .txt-kor").css("font-family","AppleSDGothicNeoM");
     });
     
+    
+    
+    
+    //사이드 메뉴 활성화
+    
+    $(".side-menu-button > a > .arrow-right").click(function(){
+       $("#side-menu").css("width","350px");
+        
+        $(".side-menu-button > a > .arrow-right").css("display","none");
+        $(".side-menu-button > a > .arrow-left").css("display","block");
+        $(".side-menu-button").css("left","350px");
+        
+        
+        $("#bg-black").css("display","block");
+        
+        $("body").css("overflow-y","hidden");
+    });
+    
+    
+    
+    // 사이드 메뉴 비활성화 
+    
+    $(".side-menu-button > a > .arrow-left").click(function(){
+       $("#side-menu").css("width","0px");
+        
+        $(".side-menu-button > a > .arrow-right").css("display","block");
+        $(".side-menu-button > a > .arrow-left").css("display","none");
+        $(".side-menu-button").css("left","0px");
+        
+        $("#bg-black").css("display","none");
+        
+        $("body").css("overflow-y","scroll");
+    });
+    
 });

bf7076bcbe0f1b8787d415d8ca70087c7fbce698
piljupark <62000236+piljupark@users.noreply.github.com>
2021-09-11 13:04:21 +0900

    210911-2

--- plantlance-script/gnb.js
+++ plantlance-script/gnb.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-/* 메뉴 - 슬라이드 메뉴 활성화 */
+/* 메뉴 - 웹 슬라이드 메뉴 활성화 */
 $("nav > .nav-wrap > a > .menu-icon").click(function () {
     $("nav > .nav-wrap > a > .menu-icon").css("opacity","0");
     $("nav > .nav-wrap > a > .menu-icon").css("transition-duration",".1s");
@@ -14,7 +14,7 @@ $("nav > .nav-wrap > a > .menu-icon").click(function () {
     
     $("#nav-slide > .nav-slide-wrap > .nav-slide-side > .side-wrap > a > .close-button").css("display","block");
 });
-/* 메뉴 - //슬라이드 메뉴 활성화 */
+/* // 메뉴 - 웹 슬라이드 메뉴 활성화 */
 
 
 
@@ -58,6 +58,45 @@ $("#nav-slide > .nav-slide-wrap > .nav-main-menu > .shop > .shop-wrap > .furnitu
 
 
 
+    
+    
+    
+    
+    /* 메뉴 - 모바일 슬라이드 메뉴 활성화 */
+$("nav > .nav-wrap > a > .menu-icon").click(function () {
+    $("nav > .nav-wrap > a > .menu-icon").css("opacity","0");
+    $("nav > .nav-wrap > a > .menu-icon").css("transition-duration",".1s");
+    $("nav > .nav-wrap > a > .menu-icon").css("transition-delay","0s");
+    
+    $(".nav-mob").css("width", "100%");
+    $(".nav-mob").css("z-index", "5");
 
+    $(".nav-mob > .nav-mob-bottom").css("transition-duration", ".2s");
+    $(".nav-mob > .nav-mob-bottom").css("transition-delay", ".3s");
+    $(".nav-mob > .nav-mob-bottom").css("opacity", "1");
+    
+    $(".nav-mob > .nav-mob-top > a > .close-button").css("display","block");
+});
+/* // 메뉴 - 모바일 슬라이드 메뉴 활성화 */
+    
+    
+    /* 메뉴 - 닫기 - 슬라이드 메뉴 비활성화 */
+$(".nav-mob > .nav-mob-top > a > .close-button").click(function () {
+    $("nav > .nav-wrap > a > .menu-icon").css("opacity","1");
+    $("nav > .nav-wrap > a > .menu-icon").css("transition-delay",".4s");
+    $("nav > .nav-wrap > a > .menu-icon").css("transition-duturation",".2s");
+    
+    $(".nav-mob").css("width", "0");
+    $(".nav-mob").css("z-index", "4");
+    
+    $(".nav-mob > .nav-mob-top > a > .close-button").css("display","none");
+    
+    $(".nav-mob > .nav-mob-bottom").css("transition-duration", ".2s");
+    $(".nav-mob > .nav-mob-bottom").css("transition-delay", ".3s");
+    $(".nav-mob > .nav-mob-bottom").css("opacity", "1");
+
+/* 메뉴 - 닫기 - 슬라이드 메뉴 비활성화 */
+
+});
 });
 

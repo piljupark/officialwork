b512de3655eb390c861cfa8bf6a3f38dedd2dd31
piljupark <62000236+piljupark@users.noreply.github.com>
2021-10-06 12:24:24 +0900

    211006-2

--- plantlance-script/wily-detail-mob.js
+++ plantlance-script/wily-detail-mob.js
@@ -1,24 +1,22 @@
 $(document).ready(function () {
 
-  $(".mob-option-up").click(function(){
-      $(".mob-option-wrap").css("height","500px");
-      $(".mob-option-up").css("display","none");
-      $(".mob-option-down").css("display","block");
-  });
-    
-    
-    $(".mob-option-down").click(function(){
-      $(".mob-option-wrap").css("height","80px");
-      $(".mob-option-up").css("display","block");
-      $(".mob-option-down").css("display","none");
-  });
-
-    
-    
-    $(".mob-product-more a").click(function(){
-        $(".mob-product-img-wrap").css("height","auto");
-        $(".mob-product-more").css("display","none");
+    $(".mob-option-up").click(function () {
+        $(".mob-option-wrap").css("height", "500px");
+        $(".mob-option-up").css("display", "none");
+        $(".mob-option-down").css("display", "block");
     });
 
 
+    $(".mob-option-down").click(function () {
+        $(".mob-option-wrap").css("height", "80px");
+        $(".mob-option-up").css("display", "block");
+        $(".mob-option-down").css("display", "none");
+    });
+
+
+
+    $(".mob-product-more a").click(function () {
+        $(".mob-product-img-wrap").css("height", "auto");
+        $(".mob-product-more").css("display", "none");
+    });
 });

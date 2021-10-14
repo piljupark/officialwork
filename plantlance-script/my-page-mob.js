e03ecfbb33b370f0f41cdf1e8f7df8f70c4c3ca6
piljupark <62000236+piljupark@users.noreply.github.com>
2021-10-14 18:42:14 +0900

    211014-4

--- plantlance-script/my-page-mob.js
+++ plantlance-script/my-page-mob.js
@@ -10,4 +10,21 @@ $(document).ready(function () {
         
         $(".mob-pw-change-wrap").css("display","none");
     });
+    
+    
+    
+    
+    $(".mob-user-delete").click(function(){
+        $("html").css("overflow-y","hidden");
+        
+        $(".mob-user-delete-wrap").css("display","block");
+    });
+    
+    $(".mob-user-delete-cancel").click(function(){
+        $("html").css("overflow-y","auto");
+        
+        $(".mob-user-delete-wrap").css("display","none");
+    });
+    
+    
 });
